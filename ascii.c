d73703043728b074e0fcb25b61f17e2e9af9455a
linkslice <sparctacus@gmail.com>
2012-06-23 21:57:06 -0700

    messing around with stuff.  Committing while it works. :-)

--- ascii.c
+++ ascii.c
@@ -10,27 +10,25 @@
 
 int main()
 {
-	int start = 'A';
-	int stop = 'Z';
-	int letter;
+	int letter, start, stop;
+	letter = start 	= 'A';
+	stop 		= 'Z';
 	int lower;
 	int cr = 1;
 
-	letter = start;
+
 	while (letter <= stop) {
 		putchar(letter);
 		printf(" = %d\t", letter);
 
-		lower = letter + 'a' - 'A';
+		lower = letter++ + 'a' - 'A';
+
 		putchar(lower);
 		printf(" = %d\t", lower);
 		
-		if (cr++ % 4 == 0) printf("\n");
+		if (cr++ % 2 == 0) printf("\n");
 
-		letter++;
 	}
-
-	printf("\n");
 
 	return 0;
 
